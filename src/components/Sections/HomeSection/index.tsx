f2e4f544a2f0a056cf0d05949e61d9ace58240e7
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-06 10:51:23 +0200

    seo tag google

--- src/components/Sections/HomeSection/index.tsx
+++ src/components/Sections/HomeSection/index.tsx
@@ -8,6 +8,7 @@ import Navigation from '@/components/Navigation';
 import Name from '@/components/Name';
 import JobContent from '@/components/JobContent';
 import { useIntersection } from '@/hooks/index';
+import MotionDiv from '@/components/Motions/MotionDiv';
 
 const HomeSection = () => {
   const { visible, add: [ref] } = useIntersection();
@@ -22,15 +23,16 @@ const HomeSection = () => {
   }, [visible]);
 
   return (
-    <div id="home" ref={ref} className="mt-10 flex-col-1 lg:flex-row-1 lg:mt-0">
-      <div className="justify-center flex-col-1 sm:items-center lg:items-start">
-        <Name />
-        <Navigation />
-      </div>
-      <div className="justify-center mx-auto mt-5 flex-col-1 sm:items-center">
-        <h1 className="mx-auto my-10 text-2xl font-AsapItal lg:mb-10">Experience</h1>
-        <div className="flex flex-row overflow-auto w-80 sm:w-96 lg:w-full lg:mx-auto lg:flex-col">
-          {
+    <MotionDiv>
+      <div id="home" ref={ref} className="mt-10 flex-col-1 lg:flex-row-1 lg:mt-0">
+        <div className="justify-center flex-col-1 sm:items-center lg:items-start">
+          <Name />
+          <Navigation />
+        </div>
+        <div className="justify-center mx-auto mt-5 flex-col-1 sm:items-center">
+          <h1 className="mx-auto my-10 text-2xl font-AsapItal lg:mb-10">Experience</h1>
+          <div className="flex flex-row overflow-auto w-80 sm:w-96 lg:w-full lg:mx-auto lg:flex-col">
+            {
             jobs_lan_en.map((job) => (
               <Link key={job.id} href="/experience/[experienceId]" as={`/experience/${job.id}`} passHref>
                 <a>
@@ -41,9 +43,10 @@ const HomeSection = () => {
               </Link>
             ))
           }
+          </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 };
 
