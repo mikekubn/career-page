927ace270c0bd83d2e53b777971e5c37e2512dc7
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-06 14:26:31 +0200

    add: motion div

--- src/components/Motions/MotionDiv/index.tsx
+++ src/components/Motions/MotionDiv/index.tsx
@@ -1,38 +1,22 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { useMediaQueries } from '@react-hook/media-query';
 
-const MotionDiv = ({ children }: { children: React.ReactNode }): React.ReactElement => {
-  const { matches: isMobile } = useMediaQueries({
-    screen: 'screen',
-    width: '(max-width: 400px)',
-  });
+const MotionDiv = ({ children }: { children: React.ReactNode }): React.ReactElement => (
+  <motion.div
+    initial={{ y: '-100vh', opacity: 0 }}
+    animate={{
+      y: '0vh',
+      opacity: 1,
+      transition: {
+        duration: 1, type: 'tween',
+      },
+    }}
+    exit={{ y: '100vh', opacity: 0 }}
+    className="flex flex-1"
+  >
+    {children}
 
-  if (!isMobile.width) {
-    return (
-      <motion.div
-        initial={{ y: '-100vh', opacity: 0 }}
-        animate={{
-          y: '0vh',
-          opacity: 1,
-          transition: {
-            duration: 1, type: 'tween',
-          },
-        }}
-        exit={{ y: '100vh', opacity: 0 }}
-        className="flex flex-1"
-      >
-        {children}
-
-      </motion.div>
-    );
-  }
-
-  return (
-    <div className="flex flex-1">
-      {children}
-    </div>
-  );
-};
+  </motion.div>
+);
 
 export default MotionDiv;
