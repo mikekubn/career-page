f3ffb63ea8a6f095edb2f264228344fcb529f62d
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-06 11:03:46 +0200

    add: remove motion on mobile

--- src/components/Motions/MotionDiv/index.tsx
+++ src/components/Motions/MotionDiv/index.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { useMediaQueries } from '@react-hook/media-query';
 
-const MotionDiv = ({ children }: { children: React.ReactNode }): React.ReactElement => (
-  <motion.div
-    initial={{ y: '-100vh', opacity: 0 }}
-    animate={{
-      y: '0vh',
-      opacity: 1,
-      transition: {
-        duration: 1, type: 'tween',
-      },
-    }}
-    exit={{ y: '100vh', opacity: 0 }}
-    className="flex flex-1"
-  >
-    {children}
-  </motion.div>
-);
+const MotionDiv = ({ children }: { children: React.ReactNode }): React.ReactElement => {
+  const { matches: isMobile } = useMediaQueries({
+    screen: 'screen',
+    width: '(max-width: 400px)',
+  });
+
+  if (!isMobile.width) {
+    return (
+      <motion.div
+        initial={{ y: '-100vh', opacity: 0 }}
+        animate={{
+          y: '0vh',
+          opacity: 1,
+          transition: {
+            duration: 1, type: 'tween',
+          },
+        }}
+        exit={{ y: '100vh', opacity: 0 }}
+        className="flex flex-1"
+      >
+        {children}
+
+      </motion.div>
+    );
+  }
+
+  return (
+    <div className="flex flex-1">
+      {children}
+    </div>
+  );
+};
 
 export default MotionDiv;
