24d1aa46d74c0ee8d99b12ad56dcbde884d51e71
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-07 22:43:38 +0200

    add: favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,10 @@ export default class MyDocument extends Document {
           <meta name="google-site-verification" content="JKVyi3BBSq744RDL7GjbV4PnS8NyES3hY93rQMu-QsE" />
           <meta name="description" content="Michael Kubín career web" />
           <meta property="og:description" content="Michael Kubín, react, next, nuxt, javascript, typescript" />
-          <link rel="icon" type="image/png" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body className="bg-white dark:bg-gray900 ease-in-out duration-700 text-gray500">
           <Main />
