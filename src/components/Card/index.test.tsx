037448b6e0fbe2ebc5723d0c510f2332bce88e7a
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-10 18:07:21 +0200

    add: card unit tests

--- src/components/Card/index.test.tsx
+++ src/components/Card/index.test.tsx
@@ -1,6 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Card from './index';
 
-it('card', () => {
-  render(<Card><div>Foo</div></Card>);
+describe('Card', () => {
+  it('render card', async () => {
+    const handleClick = jest.fn();
+    render(<Card handleClick={handleClick}>Foo</Card>);
+
+    expect(screen.getByText('Foo')).toBeVisible();
+    fireEvent.click(screen.getByTestId('card'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
 });
