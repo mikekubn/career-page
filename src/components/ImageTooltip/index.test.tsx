aa2b25940d613f16ce55c7b03b3713ba39f28014
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-17 16:25:16 +0200

    add: tests

--- src/components/ImageTooltip/index.test.tsx
+++ src/components/ImageTooltip/index.test.tsx
@@ -4,9 +4,13 @@ import ImageTooltip from '.';
 describe('Image tooltip', () => {
   it('render image tooltip', () => {
     render(
-      <ImageTooltip root="/root/test.jpg" alt="test_alt" tooltip="Test" positon="center" />,
+      <ImageTooltip root="root/test.jpg" alt="test_alt" tooltip="Test" positon="center" />,
     );
 
-    screen.debug();
+    expect(screen.getByTestId('image-tooltip-img')).toHaveAttribute('alt', 'test_alt');
+    expect(screen.getByTestId('image-tooltip-img')).toHaveAttribute('src', '/img/root/test.jpg.png');
+
+    expect(screen.getByTestId('image-tooltip-tooltip')).toHaveClass('mt-2 text-xs mx-auto sm:text-sm md:text-sm lg:text-sm');
+    expect(screen.getByTestId('image-tooltip-tooltip')).toHaveTextContent('Test');
   });
 });
--- utils/setupTests.ts
+++ utils/setupTests.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+const mockIntersectionObserver = jest.fn().mockReturnValue({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+});
+
+window.IntersectionObserver = mockIntersectionObserver;
