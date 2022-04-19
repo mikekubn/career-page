39503f05a2af199239671440639054d5fc6e583a
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-19 16:43:29 +0200

    add: Back to top

--- src/components/BackToTop/index.tsx
+++ src/components/BackToTop/index.tsx
@@ -9,6 +9,12 @@ const BackToTop = (): React.ReactElement | null => {
   if (num > 200) {
     return (
       <motion.div
+        animate={{
+          scale: [1, 1.5, 1],
+        }}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 1.2 }}
+        transition={{ duration: 0.5 }}
         className="p-2 cursor-pointer w-full fixed flex justify-end md:justify-center lg:justify-center bottom-20"
       >
         <Image src="/img/top-arrow.png" height="40" width="40" onClick={() => window.scrollTo(0, 0)} alt="Arrow top" />
