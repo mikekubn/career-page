da620b082416ff95f552de225b6d645a2b772deb
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-19 19:51:18 +0200

    fix: hot fix top arrow

--- src/components/BackToTop/index.tsx
+++ src/components/BackToTop/index.tsx
@@ -10,14 +10,14 @@ const BackToTop = (): React.ReactElement | null => {
     return (
       <motion.div
         animate={{
-          scale: [1, 1.5, 1],
+          scale: [1, 1.3, 1],
         }}
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 1.2 }}
         transition={{ duration: 0.5 }}
         className="p-2 cursor-pointer w-full fixed flex justify-end md:justify-center lg:justify-center bottom-20"
       >
-        <Image src="/img/top-arrow.png" height="40" width="40" onClick={() => window.scrollTo(0, 0)} alt="Arrow top" />
+        <div className="flex items-center justify-center w-10 h-10 bg-sky500/50 rounded-full hover:scale-110">
+          <Image src="/img/top-arrow.png" height="30" width="30" onClick={() => window.scrollTo(0, 0)} alt="Arrow top" />
+        </div>
       </motion.div>
     );
   }
