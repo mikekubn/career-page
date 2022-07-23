32559470c990ecba998686bdc0f3f06276c46dc9
Michael Kubin <michael.kubn@gmail.com>
2022-07-23 14:41:37 +0200

    add: theme next js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,18 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import MainLayout from 'src/layouts/MainLayout';
 import { NotificationProvider } from 'src/providers/NotificationProvider';
+import { ThemeProvider } from 'next-themes';
 import BackToTop from '@/components/BackToTop';
 
 const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => (
-  <NotificationProvider>
-    <BackToTop />
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
-  </NotificationProvider>
+  <ThemeProvider attribute="class">
+    <NotificationProvider>
+      <BackToTop />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </NotificationProvider>
+  </ThemeProvider>
 );
 
 export default MyApp;
