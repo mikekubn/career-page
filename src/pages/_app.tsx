5d93ca18b171c2b16d7d626d92a7647ab777eaf6
Michael Kubin <michael.kubn@gmail.com>
2022-07-23 11:36:48 +0200

    remove: unnecesary components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import MainLayout from 'src/layouts/MainLayout';
-import Footer from 'src/components/Footer';
-import Header from 'src/components/TopPanel';
 import { NotificationProvider } from 'src/providers/NotificationProvider';
 import BackToTop from '@/components/BackToTop';
 
 const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => (
   <NotificationProvider>
-    <Header />
     <BackToTop />
     <MainLayout>
       <Component {...pageProps} />
     </MainLayout>
-    <Footer />
   </NotificationProvider>
 );
 
