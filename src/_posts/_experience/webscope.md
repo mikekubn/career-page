fdaa7b15ff9b79d3d2d8363b9f3448159e9bf867
Michael Kubin <michael.kubn@gmail.com>
2022-09-04 19:14:11 +0200

    add: about

--- src/_posts/_experience/webscope.md
+++ src/_posts/_experience/webscope.md
@@ -4,5 +4,10 @@ title: Webscope
 from: 06/2022
 where: Brno, Czech Republic
 position: React Developer
-description: ['React, Next, GraphQL']
+description:
+  [
+    'My main stack - React, React router, Typescript, GraphQl, QA, Visx and Next.js.',
+    'React Developer for Observability Platform (Data flow monitoring) for a big technology company.',
+    'Mentoring for junior colleagues Tailwind css, Next.js, React, SWR for other small projects.',
+  ]
 ---
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -8,8 +8,9 @@ import Header from '@/layouts/Header';
 import MainLayout from '@/layouts/Layout';
 import { NextPageWithLayout } from '../_app';
 import Image from 'next/image';
-import { H1, Paragraph } from '@/components/Typography';
+import { H1, H2, Paragraph } from '@/components/Typography';
 import RotateList from '@/components/RotateList';
+import { MiniCard } from '@/components/Card';
 
 type Props = {
   posts: IExperience[];
@@ -25,15 +26,20 @@ const About: NextPageWithLayout<Props> = ({ posts }) => {
           <meta property="og:description" content={`experience, ${frontmatter.description}`} />
         </Head>
       ))}
-      <section className="flex flex-col m-auto">
-        <div className="flex flex-col lg:flex-row mb-5">
-          <div className="flex-col m-auto mb-10">
+      <section className="flex flex-col m-auto mt-10 sm:mt-20">
+        <div className="flex flex-col lg:flex-row mb-10">
+          <div className="flex-col m-auto mb-10 lg:mb-0">
             <Image alt="profile-image" src="career_page/profile/home_photo_circle_k9t68o" width="250" height="250" className="rounded-full" />
           </div>
           <div className="flex-col mx-auto sm:mx-20 w-72 sm:w-96 my-auto">
-            <h1 className="uppercase mb-3 text-xl font-semibold">
+            <h1 className="uppercase mb-1 text-xl font-semibold">
               Michael <span className="text-blue">Kubin</span>
             </h1>
+            <Paragraph className="mb-2 cursor-pointer hover:underline">
+              <a href="https://arc.dev/@mikekubn" target="_blank">
+                <span className="text-blue">Arc</span> Certified Remote Developer
+              </a>
+            </Paragraph>
             <RotateList className="flex justify-start box-content cursor-default mb-4" titleSize="text-lg" />
             <Paragraph>
               Hi everyone, I&apos;m <span className="text-blue font-semiBold">Michael Kubín</span> and I&apos;m a frontend developer mostly working
@@ -42,7 +48,7 @@ const About: NextPageWithLayout<Props> = ({ posts }) => {
             </Paragraph>
           </div>
         </div>
-        <div className="flex flex-col m-auto my-4">
+        <div className="flex flex-col m-auto mb-10">
           <div className="flex justify-start box-content cursor-default mb-4">
             <H1 className="uppercase">My Stack: </H1>
             <div className="pl-2 overflow-y-hidden h-8 text-lg">
@@ -54,14 +60,15 @@ const About: NextPageWithLayout<Props> = ({ posts }) => {
             </div>
           </div>
         </div>
+        {/* {posts.map(({ frontmatter }) => (
+          <MiniCard key={frontmatter.id} item={frontmatter} />
+        ))} */}
       </section>
     </>
   );
 };
 
 export default About;
-
-const stack = ['Frontend Development 👨‍💻', 'End to End testing 🧪🪛', 'Cypress', 'React 🏢', 'TypeScript ❤️', 'Next.js 🏎', 'Frontend Development 👨‍💻'];
 
 About.getLayout = function getLayout(page: React.ReactElement) {
   return (
@@ -71,6 +78,8 @@ About.getLayout = function getLayout(page: React.ReactElement) {
     </>
   );
 };
+
+const stack = ['Frontend Development 👨‍💻', 'End to End testing 🧪🪛', 'Cypress', 'React 🏢', 'TypeScript ❤️', 'Next.js 🏎', 'Frontend Development 👨‍💻'];
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const profileImages = (await cloudinary.api.resources_by_tag('work', { max_results: 20 })).resources
