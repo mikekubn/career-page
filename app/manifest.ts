a9f9ff8d8f6fa319ae511f63531562db2b0e337d
Michael Kubin <michael.kubn@gmail.com>
2024-10-10 15:43:47 +0200

    fix: manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,18 +13,20 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: '/favicon.ico',
-        sizes: 'any',
+        sizes: '48x48',
         type: 'image/x-icon',
       },
       {
-        src: '/apple-touch-icon.png',
-        sizes: '<generated>',
-        type: 'image/<generated>',
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
       },
       {
-        src: '/icon.png',
-        sizes: '<generated>',
-        type: 'image/<generated>',
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
