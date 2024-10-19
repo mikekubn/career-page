9ef1a18b5ebe391972a34dc48ca6b211f2afb357
Michael Kubin <michael.kubn@gmail.com>
2024-10-19 15:07:54 +0200

    prose invert

--- components/Article.tsx
+++ components/Article.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 const Article: React.FC<{ markdown: React.ReactElement }> = ({ markdown }) => {
   return (
     <article
-      className={clsx('prose max-w-screen-lg', {
+      className={clsx('prose prose-invert max-w-screen-lg', {
         'prose-img:-mb-4': true,
         'prose-h3:font-beVietnamPro prose-h3:font-semibold prose-h3:text-[28px] prose-h3:md:text-[34px] prose-h3:text-white': true,
         'prose-h4:font-beVietnamPro prose-h4:font-semibold prose-h4:text-[24px] prose-h4:md:text-[30px] prose-h4:text-white': true,
