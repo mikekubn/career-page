6679a9df34371e120d3ea54522862896763f91f0
Michael Kubin <michael.kubn@gmail.com>
2024-10-19 15:00:55 +0200

    fix: prose ul

--- components/Article.tsx
+++ components/Article.tsx
@@ -11,7 +11,7 @@ const Article: React.FC<{ markdown: React.ReactElement }> = ({ markdown }) => {
         'prose-p:font-inter prose-p:font-normal prose-p:text-[18px] prose-p:md:text-[22px] prose-p:text-white': true,
         'prose-a:font-inter prose-a:font-light prose-a:text-blue prose-a:text-[18px] prose-a:md:text-[22px] prose-a:no-underline hover:prose-a:underline':
           true,
-        'prose-ul:font-inter prose-ul:font-normal prose-ul:text-[18px] prose-ul:md:text-[22px]prose-ul:text-white': true,
+        'prose-ul:font-inter prose-ul:font-normal prose-ul:text-[18px] prose-ul:md:text-[22px] prose-ul:text-white': true,
       })}>
       {markdown}
     </article>
