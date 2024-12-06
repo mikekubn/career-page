b0ed94fe09513e289c053d86cf8f565065b9c4b9
Michael Kubin <michael.kubn@gmail.com>
2024-12-06 17:37:58 +0100

    remove: link to mp4

--- components/Video.tsx
+++ components/Video.tsx
@@ -33,7 +33,7 @@ const Video = () => {
       autoPlay={true}
       playsInline={true}
       className="rounded-[20px]">
-      <source src="https://res.cloudinary.com/dctc6iyms/video/upload/v1727121296/akcenta_d3wrqm.mp4" type="video/mp4" />
+      <source src="/akcenta_d3wrqm.mp4" type="video/mp4" />
     </motion.video>
   );
 };
