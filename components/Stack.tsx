f039e73c8709ef9973585905ec01f192d86e6711
Michael Kubin <michael.kubn@gmail.com>
2025-03-23 15:01:50 +0100

    fix: ts ignore

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 'use client';
 
 import React from 'react';
@@ -16,18 +15,28 @@ import StrapiSvg from '@/public/technologies/strapi.svg';
 import clsx from 'clsx';
 
 const technologies = [
+  //@ts-ignore
   { title: 'React', src: <ReactSvg className="size-[50px] md:size-[60px]" /> },
+  //@ts-ignore
   { title: 'Next.js', src: <NextJsSvg className="size-[50px] md:size-[60px]" /> },
+  //@ts-ignore
   { title: 'TypeScript', src: <TypeScriptSvg className="size-[50px] md:size-[60px]" /> },
+  //@ts-ignore
   { title: 'JavaScript', src: <JavaScriptSvg className="size-[50px] md:size-[60px]" /> },
+  //@ts-ignore
   { title: 'Tailwind CSS', src: <TailwindCssSvg className="size-[50px] md:size-[60px]" /> },
+  //@ts-ignore
   { title: 'HTML', src: <HtmlSvg className="size-[50px] md:size-[60px]" /> },
+  //@ts-ignore
   { title: 'DigitalOcean', src: <DigitalOceanSvg className="size-[90px] md:size-[120px]" /> },
 ];
 
 const services = [
+  //@ts-ignore
   { title: 'Vercel', src: <VercelSvg className="h-[80px] w-[140px] md:h-[80px] md:w-[160px]" /> },
+  //@ts-ignore
   { title: 'Prisma', src: <PrismaSvg className="h-[80px] w-[140px] md:h-[80px] md:w-[160px]" /> },
+  //@ts-ignore
   { title: 'Strapi', src: <StrapiSvg className="h-[80px] w-[140px] md:h-[80px] md:w-[160px]" /> },
 ];
 
